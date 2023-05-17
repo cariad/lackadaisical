d9b061747d4c15dd1d51e049fcf919a1dbc2a2d9
Cariad Eccleston <cariad@cariad.earth>
2023-05-17 17:39:59 +0100

    Add assert_faster_than and TooSlow

--- lackadaisical/__init__.py
+++ lackadaisical/__init__.py
@@ -27,5 +27,13 @@ You can find me at [cariad.earth](https://www.cariad.earth),
 
 from importlib.resources import open_text
 
+from lackadaisical.faster_than import assert_faster_than
+from lackadaisical.too_slow import TooSlow
+
 with open_text(__package__, "VERSION") as t:
     __version__ = t.readline().strip()
+
+__all__ = [
+    "TooSlow",
+    "assert_faster_than",
+]
